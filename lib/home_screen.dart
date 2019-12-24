37d806fa096b731baf94870848bde05437a433ed
Harsh Yadav <harshyadav94250@gmail.com>
2019-12-24 19:33:02 +0530

    Initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:best_flutter_ui_templates/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'model/homelist.dart';
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  List<HomeList> homeList = HomeList.homeList;
+  AnimationController animationController;
+  bool multiple = true;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    super.initState();
+  }
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 0));
+    return true;
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppTheme.white,
+        body: FitnessAppHomeScreen()
+
+    );
+  }
+}
